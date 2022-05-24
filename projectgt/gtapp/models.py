257c361fb05ab04fd3e20a0b3878ebe462e21674
FernandoGarzonT <fernandogarto@gmail.com>
2022-05-24 18:15:39 -0500

    create Imgen model and modifed IndexView

--- projectgt/gtapp/models.py
+++ projectgt/gtapp/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+class Imagen(models.Model):
+    """Create Model Imagen"""
+
+    name_imagen = models.CharField(max_length=50, help_text='Name description')
+    image_location = models.ImageField(upload_to='images/', blank=True, null=True)
+    text_imagen = models.TextField(max_length=10000)
+    date_create_imagen = models.DateTimeField('Date Create Imagen')
+
+    def __str__(self):
+        return self.name_imagen
+        
--- projectgt/gtapp/views.py
+++ projectgt/gtapp/views.py
@@ -1,6 +1,12 @@
+from re import I
 from django.shortcuts import render
 from django.views import generic
+from .models import Imagen
 
 
 class IndexView(generic.ListView):
     template_name = 'index.html'
+    model = Imagen
+
+    def get_queryset(self):
+        return Imagen.objects.all()
