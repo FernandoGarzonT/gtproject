07b2b5cb9b225c319842a1e226250ebef2af50b5
FernandoGarzonT <fernandogarto@gmail.com>
2022-06-01 14:17:50 -0500

    Files modified urls, views two functions added palindrome and, Odd or even, games.html and games.css

--- projectgt/gtapp/urls.py
+++ projectgt/gtapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import IndexView, ImagenView, GamesView
+from .views import IndexView, ImagenView, palindromo, parorimpar
 
 
 app_name = 'gtapp'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('imagen/', ImagenView.as_view(), name='imagen'),
-    path('games/', GamesView.as_view(), name='games'),
+    path('games/', parorimpar, name='games'),
+    path('games/', palindromo, name='games'),
 ]
--- projectgt/gtapp/views.py
+++ projectgt/gtapp/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 from .models import Imagen
@@ -19,38 +20,32 @@ class ImagenView(generic.ListView):
     def get_queryset(self):
         return Imagen.objects.all()
 
+  
+def palindromo(request):
+    """Palindrome function"""
+    if request.method == 'POST':
+        palabra = request.POST.get('palabra', '')
 
-class GamesView(generic.ListView):
-    template_name = 'games.html'
-    context_object_name= 'context_game'
-
-    def parorimpar(request):
-        """Odd or even function"""
-
-        if request.method == 'POST':
-            num = int(input("Escribe un Numero: "))
-
-            if num%2 == 0:
-                print(f"{num} Es Par")
-            else:
-                print(f"{num} Es Impar")
-        
-        return render(request, 'gtapp/games.html', {'title':'games'})
-
-
-    def palindromo(request):
-        """Game Palindromo"""
-
-        palabra = input("Escribe una palabra: ").upper()
-
-        invertida = palabra[::-1].lower()
+        invertida = palabra[::-1]
 
         if invertida == palabra:
-            print(f"{palabra} - {invertida} Es un Palindromo")
+            return HttpResponse("Is Palindrome")
         else:
-            print(f"{palabra} - {invertida} No es un Palindromo")
-        
-        return render(request, 'gtapp/games.html', {'title':'games'})
-    
-    def get_queryset(self):
-        return 
+            return HttpResponse("Not is Palindrome")
+
+    return render(request, 'games.html', )
+
+def parorimpar(request):
+    """Odd or even function"""
+
+    if request.method == 'POST':
+        num = request.POST.get('num', '')
+
+        num = int(num)
+
+        if num%2 == 0:
+            return HttpResponse("It's Even")
+        else:
+            return HttpResponse("It's Odd")
+            
+    return render(request, 'games.html')
